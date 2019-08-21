6df4041f1ee16a207234613df13c7962c35cb515
Nenad Panic <nenadpanic1997@icloud.com>
2019-08-21 23:02:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,8 @@ class User {
         this.password = password;
     }
 }
-// user list
-const users = [];
-// UI class
 
+// UI class
 class UI {
     // Alert
     static showAlert(message, className){
@@ -29,31 +27,39 @@ class UI {
         document.querySelector("#new-email").value = '';
         document.querySelector("#new-password").value = '';
     }
-    // Set the welcoming text in the login.html 
-    static welcomeText(){
-        var name = document.querySelector("#new-name").value;
-        document.querySelector("#welcome-text").textContent = name;
+    // New User Greeting
+    static greetings(){
+        var greetingName = document.querySelector("#new-name").value;
+        greeting = document.getElementById("greetings").innerHTML = `Welcome, ${greetingName}`;
     }
 }
+
+// user list
+const users = [];
+
+// Store class
 class Store {
     static addUserToList(user){
        users.push(user);
     }
 }
-//Take the form information and instatiate a User
+
+// EVENTS
+
+// instatiate a User
 document.querySelector("#registerBtn").addEventListener("click", (e) => {
-    // Get the information of a user
+    // Collect form values
     var name = document.querySelector("#new-name").value;
     var surname = document.querySelector("#new-surname").value;
     var email = document.querySelector("#new-email").value;
     var password = document.querySelector("#new-password").value;
-
+    //Validate form
     if(name === '' || surname === '' || email === '' || password === ''){
         UI.showAlert('You have to fill out the form', 'danger');
     }else if(password.length < 8){
         UI.showAlert('Password needs to be at least 8 characters', 'danger');
     }else {
-        //Instatiate a user
+    //Instatiate a user
     const user = new User(name, surname, email, password);
     // Add a user to the array
     Store.addUserToList(user);
@@ -65,20 +71,26 @@ document.querySelector("#registerBtn").addEventListener("click", (e) => {
     }
 }
 );
+
 // Login form
 document.querySelector('#login-btn').addEventListener('click', (e) => {
     //Get form values
     var loginEmail = document.querySelector('#email').value;
     var loginPassword = document.querySelector('#password').value;
+
     for (var i = 0; i < users.length; i++) {
         if (loginEmail == users[i].email) {
           if (loginPassword == users[i].password) {
              window.location.href = "login.html";
             return
-          }else {
-              UI.showAlert("You have typed your credentials incorrectly", "danger");
           }
+          }else {
+            UI.showAlert("You have typed your credentials incorrectly", "danger");
         }
     }
 });
+
+// Greet the registering user
+document.querySelector('#new-name').addEventListener("blur", UI.greetings);
+
 
